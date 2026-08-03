309588f971a8d5fb21c856dfb1845cb821e9a5d9
kiran <kanzariyakiran1307@gmail.com>
2026-08-03 16:45:55 +0530

    Initial commit

--- src/ML_PROJECT/utils.py
+++ src/ML_PROJECT/utils.py
@@ -3,11 +3,13 @@ import sys
 from src.ML_PROJECT import logger
 from src.ML_PROJECT.exception import customeException
 from dotenv import load_dotenv
+import pymysql
+import pandas as pd
 
 load_dotenv()
 
 HOST=os.getenv("host")
-ROOT=os.getenv("root")
+USER=os.getenv("user")
 PASSWORD=os.getenv("password")
 DB=os.getenv("db")
 
@@ -15,7 +17,9 @@ def read_sql_data():
     # print("ENV DATA :- ",HOST, ROOT, PASSWORD, DB)
     logger.logging.info("Start a reading Data From MYSQL")
     try:
-        pass
+        connection_sql=pymysql.connect(user=USER,host=HOST,password=PASSWORD,db=DB)
+        print("connrction succssfully")
+        print(connection_sql)
     except Exception as e:
         logger.logging.info(customeException(e, sys))
         raise customeException(e, sys)
